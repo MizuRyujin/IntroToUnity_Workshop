8703f22a2728454f2bf7404a36ce9736b65df12d
Miguel Fernández <miguelrf95@gmail.com>
2021-04-09 15:33:57 +0100

    Initial commit

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for all entities in the game, player, enemies, NPCs etc... 
+/// Abstract so it isn't used by mistake and inherits from MonoBehaviour so all 
+/// children classes can be used as components in editor
+/// </summary>
+public abstract class Entity : MonoBehaviour
+{
+    [SerializeField] protected bool isPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
